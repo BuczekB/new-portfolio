5d6bc038b96895e4a078794e8d09f2f9967d6cbf
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2023-07-03 23:40:44 +0200

    repair burger menu on small devices

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,61 +1,69 @@
+import { useState } from 'react'
 import './Navigation.css'
 
-import { useState } from 'react'
+
 
 
 import {Link} from 'react-router-dom'
 
 const Navigation = (props) =>{
 
-    
-    const [flag, setFlag] = useState(true)
-    const [close, setClose] = useState('')
-    const [open, setOpen] = useState('')
+
+    const [flag, setFlag] = useState(false)
+    const [isClose, setIsClose] = useState('')
+ 
 
     
+    const closeMenu = () =>{
 
+        const classOfMenu = flag ? 'unvisable' : 'isVisable'
+
+        setFlag(!flag)
+
+        console.log(classOfMenu);
+
+        setIsClose(classOfMenu)
+
+    }
+
+    const openMenu = () =>{
+
+        const classOfMenu =  'isVisable'
+
+
+        setIsClose(classOfMenu)
+
+    }
    
 
-
-const openMenu = () =>{
-
-    setOpen('isVisable')
-    
-}
-
-const closeMenu = () =>{
-
-    setOpen('unvisable')
-    
-}
 
 
 
     return(
         <div>
-            <div className={open}>
-            <div className="navContainer">
-            <nav>
-                <div className='logo'>
-                    <a href='/'>
-                {props.logo}
-                    </a></div>
-               
-                <ul onClick={closeMenu} className='navList'>
-                   <Link to='/'> <li  className='listItem'><p>Home</p></li></Link>
-                   <Link to='/About'> <li  className='listItem'><p>About</p></li></Link>
-                   <Link to='/Projects'> <li  className='listItem'><p>Projects</p></li></Link>
-                </ul>
-            </nav>
-            </div>    
-        </div>
-        <div className='isVisable'>
-                <div onClick={openMenu} className='menuOnMobile'>
-                    <svg  className='menuIcon' xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48">
-                    <path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/>
-                    </svg>
-                    </div>
-                </div>   
+                <div className={isClose}>
+                    <div className="navContainer">
+                    <nav>
+                        <div className='logo'>
+                            <a href='/'>
+                        {props.logo}
+                            </a></div>
+                    
+                        <ul className='navList'>
+                        <li onClick={closeMenu} className='listItem'><Link to='/'><p className='navItem'>Home</p></Link></li>
+                        <li  className='listItem'><Link to='/About'><p className='navItem'>About</p></Link></li>
+                        <li  className='listItem'><Link to='/Projects'><p className='navItem'>Projects</p></Link></li>
+                        </ul>
+                    </nav>
+                    </div>    
+                </div>
+                        <div className='isVisable'>
+                                <div onClick={openMenu}  className='menuOnMobile'>
+                                    <svg  className='menuIcon' xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48">
+                                    <path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/>
+                                    </svg>
+                                    </div>
+                            </div>   
         </div>
     )
 }
