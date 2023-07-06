b17074baf20235608e49150a83fe97ecfbdaa36a
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2023-07-06 12:00:10 +0200

    reapair nav in mobile

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,26 +1,30 @@
-import {   useRef } from 'react'
+import {   useState } from 'react'
 import './Navigation.css'
 
 
 
 
-import {NavLink} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Navigation = (props) =>{
 
-    const screenWidth = window.innerWidth
+    
 
-    const navRef = useRef()
+   
+ const [isMobile, setIsMobile] = useState(true)
 
     const  showNav =  () => {
 
-        
-            navRef.current.classList.toggle("unvisable") 
-        
-        
+       isMobile ? setIsMobile(false) : setIsMobile(true)
+       console.log(isMobile);
       
     }
 
+  
+    const closeMenu = () => {
+        setIsMobile(true)
+    }
+    
 
     
    
@@ -30,7 +34,6 @@ const Navigation = (props) =>{
 
     return(
         <div>
-                <div ref={navRef}>
                     <div className="navContainer">
                     <nav>
                         <div className='logo'>
@@ -39,20 +42,27 @@ const Navigation = (props) =>{
                             </a></div>
                     
                         <ul className='navList'>
-                        <li onClick={showNav} className='listItem'><NavLink to='/'><p className='navItem'>Home</p></NavLink></li>
-                        <NavLink to='/About'> <li onClick={showNav} className='listItem'><p className='navItem'>About</p></li></NavLink>
-                        <li onClick={showNav} className='listItem'><NavLink to='/Projects'><p className='navItem'>Projects</p></NavLink></li>
+                        <li className='listItem'><Link to='/'><p className='navItem'>Home</p></Link></li>
+                        <Link to='/About'> <li className='listItem'><p className='navItem'>About</p></li></Link>
+                        <li className='listItem'><Link to='/Projects'><p className='navItem'>Projects</p></Link></li>
                         </ul>
                     </nav>
                     </div>    
-                </div>
-                        <div className='isVisable'>
-                                <div onClick={showNav}  className='menuOnMobile'>
-                                    <svg  className='menuIcon' xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48">
+                            <div onClick={showNav}  className='menuOnMobile'>
+                                <svg  className='menuIcon' xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48">
                                     <path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/>
-                                    </svg>
-                                    </div>
-                            </div>   
+                                </svg>
+                            </div>
+
+                        <div className={isMobile ? 'navBoxMobile' : 'navBoxMobileHiden'}>
+                            <ul className={isMobile ? 'listBoxMobileHiden' : 'listBoxMobile'}>
+                                <Link onClick={closeMenu} to='/'><li className='singleItem'>Home</li></Link>
+                                <Link onClick={closeMenu} to='/About'><li className='singleItem'>About</li></Link>
+                                <Link onClick={closeMenu} to='/Projects'><li className='singleItem'>Projects</li></Link>
+                            </ul>
+                        </div>
+                                            
+                              
         </div>
     )
 }
